e8a2d9ab93c74dc6ba7c1b42b3f12612129aa8ce
Kiran A N <kiranan2004@gmail.com>
2026-05-01 21:57:58 +0530

    updated flask app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Pipeline with Docker Running Successfully"
+    return f"Current Server Time: {datetime.now()}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
